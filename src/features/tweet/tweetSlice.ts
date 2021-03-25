79c71d39a0576faab9471c8cc583247666c947f9
greytabby <munchkin.coco@gmail.com>
2021-03-25 22:32:27 +0900

    update

--- src/features/tweet/tweetSlice.ts
+++ src/features/tweet/tweetSlice.ts
@@ -5,37 +5,51 @@ import {Status as Tweet} from 'twitter-d'
 
 interface twitterState {
   tweets: Tweet[]
+  fetching: boolean
 }
 
 const initialState: twitterState = {
-  tweets: []
+  tweets: [],
+  fetching: false,
 }
 
 export const twitterSlice = createSlice({
   name: 'twitter',
   initialState,
   reducers: {
+    setFetching: (state, action: PayloadAction<boolean>) => {
+      state.fetching = action.payload
+    },
+    fetchTimelineStart: (state) => {
+      state.fetching = true
+    },
     fetchTimelineFailure: (state, action) => {
       console.log(action)
+      state.fetching = false
     },
     fetchTimelineSuccess: (state, action: PayloadAction<Tweet[]>) => {
       state.tweets.splice(0, state.tweets.length, ...action.payload)
+      state.fetching = false
     },
   }
 })
 
-export const { fetchTimelineFailure, fetchTimelineSuccess } = twitterSlice.actions
+export const { setFetching, fetchTimelineStart, fetchTimelineFailure, fetchTimelineSuccess } = twitterSlice.actions
 
 export const fetchTimelenAsync = (): AppThunk => async dispatch => {
   const url = `${process.env.REACT_APP_API_ENDPOINT}/tweets`
+  dispatch(fetchTimelineStart())
   try {
     const resp = await axios.get<Tweet[]>(url)
     dispatch(fetchTimelineSuccess(resp.data))
   } catch (error) {
     dispatch(fetchTimelineFailure(error))
+  } finally {
+    dispatch(setFetching(false))
   }
 }
 
 export const selectTweets = (state: RootState) => state.twitter.tweets
+export const selectFetching = (state: RootState) => state.twitter.fetching
 
 export default twitterSlice.reducer
--- src/views/HomeTimeline.tsx
+++ src/views/HomeTimeline.tsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import BaseLayout from '../layout/BaseLayout'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
-import { selectTweets, fetchTimelenAsync } from '../features/tweet/tweetSlice'
+import { selectFetching, selectTweets, fetchTimelenAsync } from '../features/tweet/tweetSlice'
 import {
   Avatar,
   Box,
@@ -13,14 +13,21 @@ import {
   CardMedia,
   CardContent,
   Divider,
+  Backdrop,
+  CircularProgress,
 } from '@material-ui/core'
-import { Entities, FullUser } from 'twitter-d'
+import Carousel from 'react-material-ui-carousel'
+import { Status, Entities, FullUser } from 'twitter-d'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     media: {
       height: 0,
       paddingTop: '56.25%', // 16:9
+    },
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
     },
   }),
 )
@@ -32,6 +39,7 @@ const HomeTimeline: React.FC<{}> = () => {
     dispatch(fetchTimelenAsync())
   }, [dispatch])
 
+  const fetching = useSelector(selectFetching)
   const tweets = useSelector(selectTweets)
 
   const generate = () => {
@@ -39,22 +47,50 @@ const HomeTimeline: React.FC<{}> = () => {
       return
     }
 
-    return tweets.map((v) => {
-      const user = v.user as FullUser
-      const generateMedia = (m: Entities) => {
-        if (m.media) {
-          return m.media.map((e) => {
-            return (
-              <CardMedia
-                className={classes.media}
-                image={e.media_url_https}
-                title="media"
-              />
-            )
-          })
-        }
+    const generateMedia = (m: Entities) => {
+      if (m.media) {
+        const elements: React.ReactElement[] = []
+        m.media.forEach((e) => {
+          const element = <CardMedia className={classes.media} image={e.media_url_https} title="media" />
+          elements.push(element)
+        })
+        return (
+          <Carousel>
+            {elements}
+          </Carousel>
+        )
       }
+    }
 
+    const generateTweet = (s: Status) => {
+      const user = s.user as FullUser
+      if (s.retweeted_status) {
+        const origStatus = s.retweeted_status
+        const origUser = s.retweeted_status.user as FullUser
+        return (
+          <>
+            <Card>
+              <CardHeader
+                avatar={
+                  <Avatar src={origUser.profile_image_url_https}>
+                  </Avatar>
+                }
+                title={origUser.name}
+                titleTypographyProps={{align: "left"}}
+                subheader={origStatus.created_at + ' Retweete by ' + user.name}
+                subheaderTypographyProps={{align: "left"}}
+              />
+              <CardContent>
+                <Typography align="left" variant="body2" color="textPrimary" component="p">
+                  {origStatus.text}
+                </Typography>
+              </CardContent>
+              {generateMedia(origStatus.entities)}
+            </Card>
+            <Divider/>
+          </>
+        )
+      }
       return (
         <>
           <Card>
@@ -65,19 +101,23 @@ const HomeTimeline: React.FC<{}> = () => {
               }
               title={user.name}
               titleTypographyProps={{align: "left"}}
-              subheader={v.created_at}
+              subheader={user.created_at}
               subheaderTypographyProps={{align: "left"}}
             />
             <CardContent>
               <Typography align="left" variant="body2" color="textPrimary" component="p">
-                {v.text}
+                {s.text}
               </Typography>
             </CardContent>
-            {generateMedia(v.entities)}
+            {generateMedia(s.entities)}
           </Card>
           <Divider/>
         </>
       )
+    }
+
+    return tweets.map((v) => {
+      return generateTweet(v)
     })
   }
   return (
@@ -87,6 +127,7 @@ const HomeTimeline: React.FC<{}> = () => {
           <Typography
             align='left'
             variant="h3"
+            color="textPrimary"
             gutterBottom
           >
             Twitter
@@ -95,6 +136,9 @@ const HomeTimeline: React.FC<{}> = () => {
         <List>
           {generate()}
         </List>
+        <Backdrop className={classes.backdrop} open={fetching}>
+          <CircularProgress color="inherit"/>
+        </Backdrop>
       </BaseLayout>
     </>
   )
